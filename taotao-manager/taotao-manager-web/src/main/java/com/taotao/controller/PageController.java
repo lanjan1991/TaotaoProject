42dd134d5e3b0f3465dd6730c1f27c6983da5353
Jack Lan <814425724@qq.com>
2016-11-16 11:14:26 +0800

    商品后台管理子页面

--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.taotao.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +13,15 @@ public class PageController {
 	@RequestMapping("/")
 	public String showIndex(){
 		return "index";
+	}
+	
+	/**
+	 * 展示其它页面
+	 * @param page
+	 * @return
+	 */
+	@RequestMapping("/{page}")
+	public String showpage(@PathVariable String page){
+		return page;
 	}
 }
