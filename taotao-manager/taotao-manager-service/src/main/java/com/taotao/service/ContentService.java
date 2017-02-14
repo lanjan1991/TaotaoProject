cfce0a5801e9908ae951e2222580a913d91d98f2
814425724@qq.com <814425724@qq.com>
2017-02-14 22:27:58 +0800

    6-3.2 内容管理

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ContentService.java
@@ -0,0 +1,9 @@
+package com.taotao.service;
+
+import com.taotao.pojo.TbContent;
+import com.taotao.result.TaotaoResult;
+
+public interface ContentService {
+
+	TaotaoResult insertContent(TbContent content);
+}
