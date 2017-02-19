fe4d6b1438f468fdd2e8c76732f841a942a31db3
814425724@qq.com <814425724@qq.com>
2017-02-19 18:13:00 +0800

    7-5.1 jedis整合spring及测试

--- taotao-rest/src/test/java/com/taotao/rest/jedis/JedisTest.java
+++ taotao-rest/src/test/java/com/taotao/rest/jedis/JedisTest.java
@@ -3,6 +3,8 @@ package com.taotao.rest.jedis;
 import java.util.HashSet;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
@@ -56,6 +58,31 @@ public class JedisTest {
 		System.out.println(string);
 		
 		cluster.close();
+	}
+	
+	/**
+	 * 单机版测试
+	 * <p>Title: testSpringJedisSingle</p>
+	 * <p>Description: </p>
+	 */
+	@Test
+	public void testSpringJedisSingle() {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext-*.xml");
+		JedisPool pool = (JedisPool) applicationContext.getBean("redisClient");
+		Jedis jedis = pool.getResource();
+		String string = jedis.get("key1");
+		System.out.println(string);
+		jedis.close();
+		pool.close();
+	}
+	
+	@Test
+	public void testSpringJedisCluster() {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext-*.xml");
+		JedisCluster jedisCluster =  (JedisCluster) applicationContext.getBean("redisClient");
+		String string = jedisCluster.get("key1");
+		System.out.println(string);
+		jedisCluster.close();
 	}
 	
 }
