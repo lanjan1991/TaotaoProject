0565c66cf8b9bdc73217ff6420f283e5ae278a5a
814425724@qq.com <814425724@qq.com>
2017-03-05 21:31:37 +0800

    11-5.2 添加购物车商品

---
+++ taotao-portal/src/main/java/com/taotao/portal/pojo/CartItem.java
@@ -0,0 +1,77 @@
+package com.taotao.portal.pojo;
+
+public class CartItem {
+
+	private long id;
+	private String title;
+	private Integer num;
+	private long price;
+	private String image;
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	/**
+	 * @return the num
+	 */
+	public Integer getNum() {
+		return num;
+	}
+	/**
+	 * @param num the num to set
+	 */
+	public void setNum(Integer num) {
+		this.num = num;
+	}
+	/**
+	 * @return the price
+	 */
+	public long getPrice() {
+		return price;
+	}
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(long price) {
+		this.price = price;
+	}
+	/**
+	 * @return the image
+	 */
+	public String getImage() {
+		return image;
+	}
+	/**
+	 * @param image the image to set
+	 */
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	@Override
+	public String toString() {
+		return "CartItem [id=" + id + ", title=" + title + ", num=" + num
+				+ ", price=" + price + ", image=" + image + "]";
+	}
+	
+}
