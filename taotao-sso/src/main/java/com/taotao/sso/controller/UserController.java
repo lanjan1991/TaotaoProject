81f06e9e7b26e1b97c0be2746defa5e35fd3ef37
814425724@qq.com <814425724@qq.com>
2017-03-05 12:24:10 +0800

    10-7 通过token查询用户信息

--- taotao-sso/src/main/java/com/taotao/sso/controller/UserController.java
+++ taotao-sso/src/main/java/com/taotao/sso/controller/UserController.java
@@ -96,4 +96,32 @@ public class UserController {
 			return TaotaoResult.build(500, ExceptionUtil.getStackTrace(e));
 		}
 	}
+	
+	/**
+	 * 通过token查询用户信息
+	 * @param token
+	 * @param callback
+	 * @return
+	 */
+	@RequestMapping("/token/{token}")
+	@ResponseBody
+	public Object getUserByToken(@PathVariable String token, String callback) {
+		TaotaoResult result = null;
+		try {
+			result = userService.getUserByToken(token);
+		} catch (Exception e) {
+			e.printStackTrace();
+			result = TaotaoResult.build(500, ExceptionUtil.getStackTrace(e));
+		}
+		
+		//判断是否为jsonp调用
+		if (StringUtils.isBlank(callback)) {
+			return result;
+		} else {
+			MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(result);
+			mappingJacksonValue.setJsonpFunction(callback);
+			return mappingJacksonValue;
+		}
+		
+	}
 }
