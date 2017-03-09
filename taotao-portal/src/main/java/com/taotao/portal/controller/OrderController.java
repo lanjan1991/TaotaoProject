2472d0928c1fe2e61cd87e0ee925ff934503dd84
814425724@qq.com <814425724@qq.com>
2017-03-09 22:18:06 +0800

    12-4.3 提交订单

--- taotao-portal/src/main/java/com/taotao/portal/controller/OrderController.java
+++ taotao-portal/src/main/java/com/taotao/portal/controller/OrderController.java
@@ -5,13 +5,17 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.taotao.common.utils.ExceptionUtil;
 import com.taotao.portal.pojo.CartItem;
+import com.taotao.portal.pojo.Order;
 import com.taotao.portal.service.CartService;
+import com.taotao.portal.service.OrderService;
 
 @Controller
 @RequestMapping("/order")
@@ -19,6 +23,9 @@ public class OrderController {
 	
 	@Autowired
 	private CartService cartService;
+	
+	@Autowired
+	private OrderService orderService;
 
 	@RequestMapping("/order-cart")
 	public String showOrderCart(HttpServletRequest request, HttpServletResponse response, Model model) {
@@ -28,5 +35,20 @@ public class OrderController {
 		model.addAttribute("cartList", list);
 		
 		return "order-cart";
+	}
+	
+	@RequestMapping("/create")
+	public String createOrder(Order order, Model model) {
+		try {
+			String orderId = orderService.createOrder(order);
+			model.addAttribute("orderId", orderId);
+			model.addAttribute("payment", order.getPayment());
+			model.addAttribute("date", new DateTime().plusDays(3).toString("yyyy-MM-dd"));
+			return "success";			
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("message", "创建订单出错，请稍后再试！");
+			return "error/exception";
+		}
 	}
 }
